c45d6a03511b390e8866ab0c7c9177b392f70715
Colter <cfretwell97@gmail.com>
2018-10-07 15:59:56 -0600

    Trial for messy BFS

--- Maze Search/src/BFS.java
+++ Maze Search/src/BFS.java
@@ -2,65 +2,163 @@ import java.util.*;
 
 public class BFS {
 
-    Queue<Object> queue = new PriorityQueue<Object>();
+    Queue<Node> queue = new PriorityQueue<Node>();
 
     Maze maze;
     int lastOptionCount = numOptions(maze.start.getXCoord(),maze.start.getYCoord());
     int parentDist =0;
-    Node lastNode;
+    Node temp;
+    Node start;
+    Node end;
 
     public BFS(Maze mazeIn){   // take in Object Maze
 
         maze = mazeIn;
+        end = new Node(maze.end.getXCoord(),maze.end.getYCoord(),null, null);
 
-
+        searchBFS(maze.start.getXCoord(),maze.start.getYCoord());
 
     }
 
-    public void recurBFS(int x, int y){
+    public Node searchBFS(int x, int y) {
+        // add start to queue
+        Node start = new Node(x,y,null,null); // need last node and final node?
 
-        if(maze.start.getYCoord()== y && maze.start.getXCoord()==x){  // Start state check
-            //Start state
-        }
-        else if(maze.end.getYCoord()== y && maze.end.getXCoord()==x){  // Goal state check
-            //Goal state
-        }
-        else if(maze.currentState[x][y] == '.'){     //already visited
+        queue.add(start);
+        boolean[] options = options(x,y);
+        int count = numOptions(x,y);   // track number of options
 
-            // check distance and compare
-            //     if shorter distance then update minPathLink
-            //    else igonr
-        }
-        else{  //not yet visited
-            boolean [] count = options(x,y);
+        while(queue.isEmpty() != true){
 
-            if(numOptions(x,y) != 1 && numOptions(x,y)!= lastOptionCount) {  // if number of options change
+            temp = queue.poll();
 
-                if(count[0]== true){
-                    Node temp = new Node(x,(y+1),lastNode, maze.end);
-                    queue.add(temp);
+            int[] next = new int[3];                    // next[0] t/f if node [1] x cord [2] y cord
+
+            if(options[0]=true){                                           //up
+                next = recurBFS(x,(y+1),count);
+                if(next[0]==3){             // check for goal state
+                    Node goal = new Node(next[1],next[2], temp,end);
+                    return(goal);           // return Goal
                 }
-                if(count[1]== true){
-                    Node temp = new Node((x+1),y,lastNode, maze.end);
-                    queue.add(temp);
-                }
-                if(count[2]== true){
-                    Node temp = new Node(x,(y-1),lastNode, maze.end);
-                    queue.add(temp);
-                }
-                if(count[3]== true){
-                    Node temp = new Node((x-1),y,lastNode, maze.end);
-                    queue.add(temp);
+                else if (next[0]==1){        // if return is node
+                    Node up = new Node(next[1],next[2], temp,end);// make new node
+                    maze.currentState[next[1]][next[2]]= 'O';     // nodes represented by O
+                    queue.add(up);    // add to queue
                 }
             }
-            // check if intersecton if yes
-            //    add to tree new node...
-            else{    // if no increment parentDist
-                maze.currentState[x][y]='.';
-                parentDist++;
-
+            if(options[1]=true){                                           //right
+                next = recurBFS((x+1),y, count);
+                if(next[0]==3){             // check for goal state
+                    Node goal = new Node(next[1],next[2], temp,end);
+                    return(goal);           // return Goal
+                }
+                else if (next[0]==1){        // if return is node
+                    Node right= new Node(next[1],next[2], temp,end);// make new node
+                    maze.currentState[next[1]][next[2]]= 'O';     // nodes represented by O
+                    queue.add(right);    // add to queue
+                }
             }
+            if(options[2]=true){                                           //down
+                next = recurBFS(x,(y-1), count);
+                if(next[0]==3){             // check for goal state
+                    Node goal = new Node(next[1],next[2], temp,end);
+                    return(goal);           // return Goal
+                }
+                else if (next[0]==1){        // if return is node
+                    Node down = new Node(next[1],next[2], temp,end);// make new node
+                    maze.currentState[next[1]][next[2]]= 'O';     // nodes represented by O
+                    queue.add(down);    // add to queue
+                }
+            }
+            if(options[3]=true){                                           //left
+                next = recurBFS((x-1),y, count);
+                if(next[0]==3){             // check for goal state
+                    Node goal = new Node(next[1],next[2], temp,end);
+                    return(goal);           // return Goal
+                }
+                else if (next[0]==1){        // if return is node
+                    Node left = new Node(next[1],next[2], temp,end);// make new node
+                    maze.currentState[next[1]][next[2]]= 'O';     // nodes represented by O
+                    queue.add(left);    // add to queue
+                }
+            }
+
+
         }
+        System.out.println("end not found return null");
+        return(null);
+    }
+
+    public int[] recurBFS(int inX, int inY, int count){  // find next node
+        //ie check number of options
+
+        int x = inX;
+        int y = inY;
+        int prevOption = count; // number of previous options
+        int options = numOptions(x,y);
+        boolean[] dir = options(x,y);
+        int[] temp = new int[3];
+        temp[1]= x;
+        temp[2]= y;
+
+        if(maze.end.getYCoord()== y && maze.end.getXCoord()==x){  // Goal state check
+            System.out.println("Goal reached!"); //Goal state
+            temp[0]= 3;
+
+            return(temp); // return 3 in temp[0]
+        }
+        switch(options){        // number of potential routes
+            case 0:             // dead end
+                temp[0]=0;
+
+                maze.currentState[x][y] = '.';
+                return(temp);
+
+            case 1:             // one way to go, no new node
+                if(dir[0]== true){  // up
+                    recurBFS(x,(y+1),1);
+                }
+                else if(dir[1]==true){  // Right
+                    recurBFS((x+1),y,1);
+                }
+                else if(dir[2]==true){
+                    recurBFS(x,(y-1),1);
+                }
+                else if(dir[3]==true){
+                    recurBFS((x-1),y,1);
+                }
+
+                break;
+
+            case 2:             // two ways to go only make node if different numOptions
+                if(options == count){    // number of options different from last location
+                    if(dir[0]== true){  // up
+                        recurBFS(x,(y+1),2);
+                    }
+                    else if(dir[1]==true){  // Right
+                        recurBFS((x+1),y,2);
+                    }
+                    else if(dir[2]==true){
+                        recurBFS(x,(y-1),2);
+                    }
+                    else if(dir[3]==true){
+                        recurBFS((x-1),y,2);
+                    }
+                }
+                else{   // make new node
+                    temp[0]=1;
+                    return(temp);
+
+                }
+                break;
+            case 3:             // three ways to go, make node
+                temp[0]=1;
+                return(temp);
+
+        }
+
+        System.out.println("issue with Options");
+        return(null);
     }
 
     public boolean[] options(int x, int y){  // read like clock 0 = 12
