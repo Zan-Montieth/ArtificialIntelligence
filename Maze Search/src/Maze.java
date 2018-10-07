741d05afaa0731e0076f1afa83d1312d7de246d0
Ethan Malo <ChampionPi@users.noreply.github.com>
2018-10-06 22:37:32 -0600

    Char array works now :D

--- Maze Search/src/Maze.java
+++ Maze Search/src/Maze.java
@@ -1,34 +1,26 @@
+import java.util.ArrayList;
 public class Maze {
 
-    //Node start = findStart();
-    //Node end = findEnd();
+//    Node start = findStart();
+//    Node end = findEnd();
     char[][] baseMaze;
     char[][] currentState;
     private int mazeWidth;
-    private int mazeLength;
+    private int mazeHeight;
 
-    public Maze(String textMaze, int inMazeWidth) {
-        mazeWidth = inMazeWidth - 1;
-        baseMaze = new char[mazeWidth][mazeWidth];
-        char[] input =  textMaze.toCharArray();
-
-        int y = 0;
-        while(((y + 1)* (mazeWidth)) < textMaze.length() - 1){
-
-            for (int x = 0; x < mazeWidth; x++) {
-                baseMaze[y][x] = input[(y * x) + x + 1];
-            }
-            y++;
-        }
-        mazeLength = y;
+    public Maze(char[][] textMaze) {
+        baseMaze=textMaze;
+        currentState = textMaze;
+        mazeWidth=textMaze[0].length;
+        mazeHeight=textMaze.length;
     }
 
     public void printMaze(){
-        for(int y = 0; y < mazeLength; y++){
-            for (int x = 0; x < mazeWidth; x++) {
+        for(int x = 0; x < mazeHeight; x++){
+            for (int y = 0; y < mazeWidth; y++) {
                 System.out.print(baseMaze[x][y] + " ");
             }
-            System.out.println();
+            System.out.print("\n");
         }
     }
 /*
