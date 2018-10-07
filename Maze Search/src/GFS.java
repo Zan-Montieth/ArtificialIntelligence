48ffd7935237873ff2cbe572ed0a5d38b9b99f95
Zan Montieth <zan.montieth@gmail.com>
2018-10-07 14:13:53 -0600

    setting up the GFS class and populating it with psuedo code

---
+++ Maze Search/src/GFS.java
@@ -0,0 +1,20 @@
+public class GFS {
+
+    Maze mazeGFS;
+    Maze solved;
+
+    public GFS(Maze inMaze){
+        mazeGFS = inMaze;
+    }
+
+    public Maze computeGFS(){
+        //TODO create a list of all points in the maze and their distance to the finish using manhattan distance
+        //manhattan distance in only moving on the straight lines no diagonals
+
+        //TODO take choice that gets us closest to the goal and then remove all options that are larger
+
+
+        return solved;
+    }
+
+}
