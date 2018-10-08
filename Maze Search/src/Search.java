a865223aa02d6b98b8a79e6a894433a7e8951320
Colter <cfretwell97@gmail.com>
2018-10-08 00:46:02 -0600

    Fix travers method, weight initalized as 0 before call

--- Maze Search/src/Search.java
+++ Maze Search/src/Search.java
@@ -1,9 +1,11 @@
 import java.util.PriorityQueue;
+import Algs4.*;
 
 public class Search {
 
     Maze maze;
     PriorityQueue<Node> queue;
+
 
 
     public void Search(Maze currentMaze) {
@@ -33,26 +35,29 @@ public class Search {
 
     }
 
-    private Node traverse(Maze inMaze, int x, int y) {
+    private Node traverse(Maze inMaze, int x, int y, Node base) {
         Node found;
+        weight++;
         if (inMaze.hashpull(x, y) !=null) {
             found = inMaze.hashpull(x, y);  // if node found
-
+            Node temp = inMaze.hashpull(x,y);
+            int baseNum = (base.getX()*100)+base.getY();
+            Edge edge = new Edge((x*100+y),baseNum, weight);
         }
         else {          //if node not found keep walking
             inMaze.currentState[x][y]= '.';
             if(inMaze.currentState[x][y+1]==' '){
-                found = traverse(inMaze,x,(y+1));
+                found = traverse(inMaze,x,(y+1), base);
 
             }
             if(inMaze.currentState[x+1][y]==' '){
-                found = traverse(inMaze,x+1,y);
+                found = traverse(inMaze,x+1,y, base);
             }
             if(inMaze.currentState[x][y-1]==' '){
-                found = traverse(inMaze,x,(y-1));
+                found = traverse(inMaze,x,(y-1), base);
             }
             else{
-                found = traverse(inMaze,x-1,y);
+                found = traverse(inMaze,x-1,y, base);
             }
         }
         return(found);
